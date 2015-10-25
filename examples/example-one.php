4820f9c2a137c9f3848f4891b8489718de78efe7
Paul Lovato <plovato@gmail.com>
2015-10-25 00:15:22 -0600

    Update example-one.php

--- examples/example-one.php
+++ examples/example-one.php
@@ -11,8 +11,3 @@ $password = getenv('DISCORD_PASSWORD');
 
 // Try log into Discord!
 $discord = new Discord($email_address, $password);
-
-header('Content-Type: application/json');
-$image = 'http://static-cdn.jtvnw.net/jtv_user_pictures/rabbitbong-profile_image-444fd6f17289a247-300x300.png';
-$kappa = $discord->api('server')->edit('105775765877989376', ['name' => 'DansGame', 'icon' => $image]);
-echo json_encode($kappa);
