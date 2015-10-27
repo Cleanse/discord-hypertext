201ef8ccd1dfb68436de478489f44c36fee0a2da
Paul Lovato <plovato@gmail.com>
2015-10-27 08:08:27 -0600

    User class updated.

--- src/Api/User.php
+++ src/Api/User.php
@@ -3,7 +3,32 @@ namespace Discord\Api;
 
 class User extends AbstractApi
 {
-    public function show($userId)
+    public function edit($username = null, $new_password = null, $avatar = null)
+    {
+        $me = $this->me();
+        $json['username'] = is_null($username) ? $me['username'] : $username;
+        $json['avatar'] = is_null($avatar) ? $me['avatar'] : $this->encodeAvatar($avatar);
+        $json['email'] = getenv('DISCORD_EMAIL');
+        $json['password'] = getenv('DISCORD_PASSWORD');
+        $json['new_password'] = is_null($new_password) ? '' : $new_password;
+        return $this->request('PATCH', 'users/@me', [
+            'json' => $json
+        ]);
+    }
+
+    private function encodeAvatar($image)
+    {
+        $type = pathinfo($image, PATHINFO_EXTENSION);
+        $data = file_get_contents($image);
+        return 'data:image/' . $type . ';base64,' . base64_encode($data);
+    }
+
+    public function me()
+    {
+        return $this->request('GET', 'users/@me');
+    }
+
+    public function view($userId)
     {
         return $this->request('get', 'users/' . $userId);
     }
@@ -15,7 +40,7 @@ class User extends AbstractApi
 
     public function avatar($userId)
     {
-        $user = $this->show($userId);
+        $user = $this->view($userId);
         return 'https://discordapp.com/api/users/' . $userId . '/avatars/' . $user['avatar'] . '.jpg';
     }
 }
