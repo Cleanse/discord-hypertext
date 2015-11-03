895baeef9440db822246c07e66bdee9bdba4506e
Paul Lovato <plovato@gmail.com>
2015-11-03 07:09:16 -0700

    User docs and updated api.

--- src/Api/User.php
+++ src/Api/User.php
@@ -11,26 +11,30 @@
 
 namespace Discord\Api;
 
+use Discord\Helpers\Image;
+
 class User extends AbstractApi
 {
-    public function edit($username = null, $new_password = null, $avatar = null)
+    /**
+     * @param $email
+     * @param $password
+     * @param null $username
+     * @param null $new_password
+     * @param null $avatar
+     * @return mixed|\Psr\Http\Message\StreamInterface
+     */
+    public function edit($email, $password, $username = null, $new_password = null, $avatar = null)
     {
+        $image = new Image();
         $me = $this->me();
         $json['username'] = is_null($username) ? $me['username'] : $username;
-        $json['avatar'] = is_null($avatar) ? $me['avatar'] : $this->encodeAvatar($avatar);
-        $json['email'] = getenv('DISCORD_EMAIL');
-        $json['password'] = getenv('DISCORD_PASSWORD');
+        $json['avatar'] = is_null($avatar) ? $me['avatar'] : $image->encodeImage($avatar);
+        $json['email'] = $email;
+        $json['password'] = $password;
         $json['new_password'] = is_null($new_password) ? '' : $new_password;
         return $this->request('PATCH', 'users/@me', [
             'json' => $json
         ]);
-    }
-
-    private function encodeAvatar($image)
-    {
-        $type = pathinfo($image, PATHINFO_EXTENSION);
-        $data = file_get_contents($image);
-        return 'data:image/' . $type . ';base64,' . base64_encode($data);
     }
 
     public function me()
@@ -38,7 +42,7 @@ class User extends AbstractApi
         return $this->request('GET', 'users/@me');
     }
 
-    public function view($userId)
+    public function show($userId)
     {
         return $this->request('get', 'users/' . $userId);
     }
@@ -50,7 +54,7 @@ class User extends AbstractApi
 
     public function avatar($userId)
     {
-        $user = $this->view($userId);
+        $user = $this->show($userId);
         return 'https://discordapp.com/api/users/' . $userId . '/avatars/' . $user['avatar'] . '.jpg';
     }
 }
